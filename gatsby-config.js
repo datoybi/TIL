8397a08ab5febc7eb283847d74b6f8b2775280ee
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-03-28 23:31:00 +0900

    :gift: feat: theme customizing

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,16 +9,60 @@ module.exports = {
         name: `github`,
         url: `https://github.com/datoybi`,
       },
-      {
-        name: `twitter`,
-        url: `https://twitter.com/`,
-      },
+      // {
+      //   name: `twitter`,
+      //   url: `https://twitter.com/HuntaroSan`
+      // }
     ],
     sidebarConfig: {
       forcedNavOrder: ["/introduction", "/codeblock"],
       ignoreIndex: true,
     },
   },
-  plugins: [{ resolve: `gatsby-theme-document`, options: {} }],
-  // pathPrefix: "/fe-tech",
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `content`,
+        name: `content`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 704,
+            },
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              icon: false,
+            },
+          },
+          `gatsby-remark-embed-video`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Document`,
+        short_name: `Document`,
+        start_url: `/`,
+        background_color: `#182952`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: "src/site-icon.png",
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-theme-ui`,
+    `gatsby-plugin-react-helmet`,
+  ],
 };
--- src/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-plugin-theme-ui/colors.js
@@ -6,22 +6,22 @@ export default {
   sidebar: '#eee',
   borderColor: 'rgba(0, 0, 0, 0.15)',
   modes: {
-    dark: {
-      text: '#fff',
-      background: '#182952',
-      primary: '#f638dc',
-      secondary: '#ff7976',
-      sidebar: '#101d3c',
-      borderColor: 'rgba(255, 255, 255, 0.15)'
-    },
-    cool: {
-      text: '#fff',
-      background: '#05386b',
-      primary: '#5cdb95',
-      secondary: '#bef992',
-      sidebar: '#052e56',
-      borderColor: 'rgba(255, 255, 255, 0.15)'
-    },
+    // dark: {
+    //   text: '#fff',
+    //   background: '#182952',
+    //   primary: '#f638dc',
+    //   secondary: '#ff7976',
+    //   sidebar: '#101d3c',
+    //   borderColor: 'rgba(255, 255, 255, 0.15)'
+    // },
+    // cool: {
+    //   text: '#fff',
+    //   background: '#05386b',
+    //   primary: '#5cdb95',
+    //   secondary: '#bef992',
+    //   sidebar: '#052e56',
+    //   borderColor: 'rgba(255, 255, 255, 0.15)'
+    // },
     deep: {
       text: '#fff',
       background: 'hsl(230,25%,18%)',
