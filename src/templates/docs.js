c5878eb98b832974dab5d865bb624a9c0b625648
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-04-08 09:35:01 +0900

    :memo: docs: this 스터디

--- src/templates/docs.js
+++ src/templates/docs.js
@@ -1,18 +1,21 @@
-import styled from '@emotion/styled';
-import { graphql } from 'gatsby';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-import PropTypes from 'prop-types';
-import React from 'react';
-import Layout from '../components/Layout';
-import SEO from '../components/SEO';
+import styled from "@emotion/styled";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import PropTypes from "prop-types";
+import React from "react";
+import Layout from "../components/Layout";
+import SEO from "../components/SEO";
 
 const DocsTemplate = ({ data, location }) => {
   const { mdx } = data;
 
   return (
     <Layout tableOfContents={mdx.tableOfContents} location={location}>
-      <SEO title={mdx.frontmatter.title} description={mdx.frontmatter.description} />
-      <Heading>{mdx.frontmatter.title}</Heading>
+      <SEO
+        title={mdx.frontmatter.title}
+        description={mdx.frontmatter.description}
+      />
+      {/* <Heading>{mdx.frontmatter.title}</Heading> */}
       <MDXRenderer>{mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -28,13 +31,13 @@ const Heading = styled.h1`
 
 DocsTemplate.propTypes = {
   data: PropTypes.shape({
-    mdx: PropTypes.object.isRequired
+    mdx: PropTypes.object.isRequired,
   }).isRequired,
-  location: PropTypes.object.isRequired
+  location: PropTypes.object.isRequired,
 };
 
 export const pageQuery = graphql`
-  query($id: String!) {
+  query ($id: String!) {
     mdx(fields: { id: { eq: $id } }) {
       body
       tableOfContents
